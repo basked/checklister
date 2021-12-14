448a5de9ba57f7f05d02a3ed5f30bfebf3f78fe9
basked <basket.baza@gmail.com>
2021-12-15 00:56:11 +0300

    Create templates based https://coreui.io/demo/free/3.4.0/

---
+++ resources/views/layouts/auth.blade.php
@@ -0,0 +1,32 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- CoreUI CSS -->
+    <link rel="stylesheet" href="https://unpkg.com/@coreui/coreui@3.4.0/dist/css/coreui.min.css"
+          crossorigin="anonymous">
+
+    <title>{{ config('app.name', 'Laravel') }}</title>
+</head>
+<body class="c-app flex-row align-items-center">
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-md-8">
+            <div class="card-group">
+                <div class="card p-4">
+                    <div class="card-body">
+                        @yield('content')
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+<script src="https://unpkg.com/@popperjs/core@2"></script>
+<script src="https://unpkg.com/@coreui/coreui@3.4.0/dist/js/coreui.min.js"></script>
+</body>
+</html>
+
