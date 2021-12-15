dec9d290bc5bbc2353b72bc266153bf1247dcee2
basked <basket.baza@gmail.com>
2021-12-16 01:44:12 +0300

    1) Add template create, edit for ChecklistGroups and Cheklists; 2) Add controllers for ChecklistGroups and Cheklists; 3) Add StoreRequests for ChecklistGroups and Cheklists;

---
+++ app/Http/Controllers/Admin/ChecklistController.php
@@ -0,0 +1,100 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+
+use App\Http\Controllers\Controller;
+use App\Http\Requests\StoreChecklistGroupRequest;
+use App\Http\Requests\StoreChecklistRequest;
+use App\Models\Checklist;
+use App\Models\ChecklistGroup;
+use Illuminate\Http\Request;
+
+/**
+ * Class ChecklistController
+ * @package App\Http\Controllers\Admin
+ */
+class ChecklistController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @param ChecklistGroup $checklistGroup
+     * @return \Illuminate\Http\Response
+     */
+    public function create(ChecklistGroup $checklistGroup)
+    {
+        return view('admin.checklists.create', compact('checklistGroup'));
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param StoreChecklistRequest $request
+     * @param ChecklistGroup $checklistGroup
+     * @return \Illuminate\Http\Response
+     */
+    public function store(StoreChecklistRequest $request, ChecklistGroup $checklistGroup)
+    {
+        $checklistGroup->checklists()->create($request->validated());
+        return redirect()->route('home');
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param int $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param ChecklistGroup $checklistGroup
+     * @param Checklist $checklist
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(ChecklistGroup $checklistGroup, Checklist $checklist)
+    {
+        return view('admin.checklists.edit', compact('checklistGroup', 'checklist'));
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param StoreChecklistRequest $request
+     * @param ChecklistGroup $checklistGroup
+     * @param Checklist $checklist
+     * @return \Illuminate\Http\Response
+     */
+    public function update(StoreChecklistRequest $request, ChecklistGroup $checklistGroup, Checklist $checklist)
+    {
+        $checklist->update($request->validated());
+        return redirect()->route('home');
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param int $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(ChecklistGroup $checklistGroup, Checklist $checklist)
+    {
+        $checklist->delete();
+        return redirect()->route('home');
+    }
+}
---
+++ database/migrations/2021_12_15_191134_create_checklist_groups_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateChecklistGroupsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('checklist_groups', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('checklist_groups');
+    }
+}
