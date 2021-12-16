7d1ff01014ba5f5240e060693dd4d7a528a24699
basked <basket.baza@gmail.com>
2021-12-16 21:06:15 +0300

    Controllers with No Docblocks - Clear conrtoller - Edit controller.model.stub

--- app/Http/Controllers/Admin/ChecklistController.php
+++ app/Http/Controllers/Admin/ChecklistController.php
@@ -3,7 +3,6 @@
 namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
-use App\Http\Requests\StoreChecklistGroupRequest;
 use App\Http\Requests\StoreChecklistRequest;
 use App\Models\Checklist;
 use App\Models\ChecklistGroup;
