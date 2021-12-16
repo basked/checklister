b3abc6d0debe4715e22f49aee4d30d903d5d949e
basked <basket.baza@gmail.com>
2021-12-17 00:11:28 +0300

    Add controller, view for model Page

--- resources/views/partials/sidebar.blade.php
+++ resources/views/partials/sidebar.blade.php
@@ -74,12 +74,15 @@
             </form>
 
         </li>
+
+
 {{--        <div class="ps__rail-x" style="left: 0px; bottom: 0px;">--}}
 {{--            <div class="ps__thumb-x" tabindex="0" style="left: 0px; width: 0px;"></div>--}}
 {{--        </div>--}}
 {{--        <div class="ps__rail-y" style="top: 0px; height: 616px; right: 0px;">--}}
 {{--            <div class="ps__thumb-y" tabindex="0" style="top: 0px; height: 508px;"></div>--}}
 {{--        </div>--}}
+
     </ul>
     <button class="c-sidebar-minimizer c-class-toggler" type="button" data-target="_parent"
             data-class="c-sidebar-minimized"></button>
