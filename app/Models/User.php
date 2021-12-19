f1be900f65fe8a394fe321f750475802cbbba9e5
basked <basket.baza@gmail.com>
2021-12-19 23:35:14 +0300

    Dekete blockcode in models

--- app/Models/User.php
+++ app/Models/User.php
@@ -12,11 +12,7 @@ class User extends Authenticatable
 {
     use HasApiTokens, HasFactory, Notifiable;
 
-    /**
-     * The attributes that are mass assignable.
-     *
-     * @var array<int, string>
-     */
+
     protected $fillable = [
         'name',
         'email',
@@ -25,21 +21,11 @@ class User extends Authenticatable
         'website',
     ];
 
-    /**
-     * The attributes that should be hidden for serialization.
-     *
-     * @var array<int, string>
-     */
     protected $hidden = [
         'password',
         'remember_token',
     ];
 
-    /**
-     * The attributes that should be cast.
-     *
-     * @var array<string, string>
-     */
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
