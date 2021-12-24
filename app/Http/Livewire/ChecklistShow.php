5af34d42f50257e561f6088602441783c36d08f5
basked <basket.baza@gmail.com>
2021-12-24 23:53:02 +0300

    Show only new task

--- app/Http/Livewire/ChecklistShow.php
+++ app/Http/Livewire/ChecklistShow.php
@@ -23,26 +23,32 @@ class ChecklistShow extends Component
 
     public function toggle_task($task_id)
     {
-         if (in_array($task_id,$this->open_tasks)){
-             // если есть id задачи в списке - удалем
-             $this->open_tasks=array_diff($this->open_tasks,[$task_id]);
-         } else {
-             // признак откытой задачи
-             $this->open_tasks[] = $task_id;
-         }
+        if (in_array($task_id, $this->open_tasks)) {
+            // если есть id задачи в списке - удалем
+            $this->open_tasks = array_diff($this->open_tasks, [$task_id]);
+        } else {
+            // признак откытой задачи
+            $this->open_tasks[] = $task_id;
+        }
 
     }
 
     public function complete_task($task_id)
     {
-        $task= Task::find($task_id);
-        if ($task){
-            $user_task = $task->replicate();
-
-            $user_task['user_id']=auth()->id();
-            $user_task['task_id']=$task_id;
-            $user_task['completed_at']=now();
-            $user_task->save();
+        $task = Task::find($task_id);
+        if ($task) {
+            $user_task = Task::where('task_id', $task_id)->first();
+            if ($user_task) {
+                if (is_null($user_task->completed_at)) {
+                    $user_task->update(['completed_at' => now()]);
+                }
+            } else {
+                $user_task = $task->replicate();
+                $user_task['user_id'] = auth()->id();
+                $user_task['task_id'] = $task_id;
+                $user_task['completed_at'] = now();
+                $user_task->save();
+            }
         }
 
     }
