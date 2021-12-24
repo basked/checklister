9783c025c502c3ed9f262248aaa60b7b03c62dc0
basked <basket.baza@gmail.com>
2021-12-25 00:00:17 +0300

    Show only new task

--- app/Http/Livewire/ChecklistShow.php
+++ app/Http/Livewire/ChecklistShow.php
@@ -20,7 +20,7 @@ class ChecklistShow extends Component
             ->whereNotNull('completed_at')
             ->pluck('task_id')->toArray();
     }
- //
+ //  dfff
     public function render()
     {
         return view('livewire.checklist-show');
