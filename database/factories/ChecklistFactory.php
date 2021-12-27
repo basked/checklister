d8a6e5f5c7f2f63e06fc6c88318acaf24fb62006
basked <basket.baza@gmail.com>
2021-12-27 20:14:19 +0300

    -Create task factory -Testing CRUD for Task

---
+++ database/factories/ChecklistFactory.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+class ChecklistFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+    public function definition()
+    {
+        return [
+            //
+        ];
+    }
+}
---
+++ database/factories/TaskFactory.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Task;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+class TaskFactory extends Factory
+{
+   protected $model = Task::class;
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+    public function definition()
+    {
+        return [
+            'name'=>$this->faker->text(20),
+            'description'=>$this->faker->text(100),
+        ];
+    }
+}
