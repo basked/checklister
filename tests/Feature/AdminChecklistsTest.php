e8ecbd36df55992c2e7292be22b8102ab79428d8
basked <basket.baza@gmail.com>
2021-12-27 20:12:48 +0300

    -Create task factory -Testing CRUD for Task

--- tests/Feature/AdminChecklistsTest.php
+++ tests/Feature/AdminChecklistsTest.php
@@ -17,12 +17,18 @@ class AdminChecklistsTest extends TestCase
 {
     use RefreshDatabase;
 
+    public function setUp(): void
+    {
+        parent::setUp();
+
+        $admin = User::factory()->create(['is_admin' => 1]);
+        $this->actingAs($admin);
+    }
+
     // Управление группами контрольных списков
     public function test_manage_checklist_grpups()
     {
-        $admin = User::factory()->create(['is_admin' => 1]);
-
-        $response = $this->actingAs($admin)->post('admin/checklist_groups', [
+        $response = $this->post('admin/checklist_groups', [
             'name' => 'First group'
         ]);
         $response->assertRedirect('welcome');
@@ -31,10 +37,10 @@ class AdminChecklistsTest extends TestCase
         $this->assertNotNull($group);
 
         // Test EDITING the checklist group
-        $response = $this->actingAs($admin)->get('admin/checklist_groups/' . $group->id . '/edit');
+        $response = $this->get('admin/checklist_groups/' . $group->id . '/edit');
         $response->assertStatus(200);
 
-        $response = $this->actingAs($admin)->put('admin/checklist_groups/' . $group->id, [
+        $response = $this->put('admin/checklist_groups/' . $group->id, [
             'name' => 'Updated first group'
         ]);
         $response->assertRedirect('welcome');
@@ -46,7 +52,7 @@ class AdminChecklistsTest extends TestCase
         $menu = (new MenuService())->get_menu();
         $this->assertEquals(1, $menu['admin_menu']->where('name', 'Updated first group')->count());
         // Test DELETING the checklist group
-        $response = $this->actingAs($admin)->delete('admin/checklist_groups/' . $group->id);
+        $response = $this->delete('admin/checklist_groups/' . $group->id);
         $response->assertRedirect('welcome');
         //Test menu after delete Checklistgroup
         $menu = (new MenuService())->get_menu();
@@ -56,24 +62,23 @@ class AdminChecklistsTest extends TestCase
     // Управление контрольными списками
     public function test_manage_checklists()
     {
-        $admin = User::factory()->create(['is_admin' => 1]);
         $checklist_group = ChecklistGroup::factory()->create();
         $checklists_url = 'admin/checklist_groups/' . $checklist_group->id . '/checklists';
         // Test CREATING the checklist
-        $response = $this->actingAs($admin)->get($checklists_url . '/create');
+        $response = $this->get($checklists_url . '/create');
         $response->assertStatus(200);
 
-        $response = $this->actingAs($admin)->post($checklists_url, ['name' => 'First checklist']);
+        $response = $this->post($checklists_url, ['name' => 'First checklist']);
         $response->assertRedirect('welcome');
 
         $checklist = Checklist::where('name', 'First checklist')->first();
         $this->assertNotNull($checklist);
         $this->assertEquals($checklist->name, 'First checklist');
         // Test EDITING the checklist
-        $response = $this->actingAs($admin)->get($checklists_url . '/' . $checklist->id . '/edit');
+        $response = $this->get($checklists_url . '/' . $checklist->id . '/edit');
         $response->assertStatus(200);
 
-        $response = $this->actingAs($admin)->put($checklists_url . '/' . $checklist->id,
+        $response = $this->put($checklists_url . '/' . $checklist->id,
             [
                 'name' => 'Updated checklist'
             ]);
@@ -86,10 +91,77 @@ class AdminChecklistsTest extends TestCase
         $this->assertTrue($menu['admin_menu']->first()->checklists->contains($checklist));
 
         // Test DELETED the checklist
-        $response = $this->actingAs($admin)->delete($checklists_url . '/' . $checklist->id);
+        $response = $this->delete($checklists_url . '/' . $checklist->id);
         $response->assertRedirect('welcome');
 
         $menu = (new MenuService())->get_menu();
         $this->assertFalse($menu['admin_menu']->first()->checklists->contains($checklist));
+    }
+
+    // Управление контрольными списками
+    public function test_manage_tasks()
+    {
+        $checklist_group = ChecklistGroup::factory()->create();
+        $checklist = Checklist::factory()->create(['name' => 'bas', 'checklist_group_id' => $checklist_group->id]);
+        // Test CREATING the task
+        $tasks_url = 'admin/checklists/' . $checklist->id . '/tasks';
+        $response = $this->post($tasks_url, [
+            'name' => 'Some task',
+            'description' => 'Some description',
+        ]);
+        $response->assertRedirect('admin/checklist_groups/' . $checklist_group->id . '/checklists/' . $checklist->id . '/edit');
+        $task = Task::where('name', 'Some task')->first();
+        $this->assertNotNull($task);
+        $this->assertEquals(1, $task->position);
+        // Test UPDATED the task
+        $response = $this->put($tasks_url . '/' . $task->id, [
+            'name' => 'Updated task',
+            'description' => $task->description
+        ]);
+        $response->assertRedirect('admin/checklist_groups/' . $checklist_group->id . '/checklists/' . $checklist->id . '/edit');
+
+        $task = Task::where('name', 'Updated task')->first();
+        $this->assertNotNull($task);
+    }
+
+    public function test_delete_task_with_position_reordered()
+    {
+        $checklist_group = ChecklistGroup::factory()->create();
+        $checklist = Checklist::factory()->create(['name' => 'Checklist1', 'checklist_group_id' => $checklist_group->id]);
+
+        $task1 = Task::factory()->create(['checklist_id' => $checklist->id, 'position' => 1]);
+        $task2 = Task::factory()->create(['checklist_id' => $checklist->id, 'position' => 2]);
+
+        $tasks_url = 'admin/checklists/' . $checklist->id . '/tasks';
+        $response = $this->delete($tasks_url . '/' . $task1->id);
+        $response->assertRedirect('admin/checklist_groups/' . $checklist_group->id . '/checklists/' . $checklist->id . '/edit');
+
+        $task = Task::where('name', $task1->name)->first();
+        $this->assertNull($task);
+
+        $task = Task::where('name', $task2->name)->first();
+        $this->assertNotNull($task);
+        $this->assertEquals(1, $task->position);
+    }
+
+    public function test_reordering_task_with_livewire()
+    {
+        $checklist_group = ChecklistGroup::factory()->create();
+        $checklist = Checklist::factory()->create(['name' => 'Checklist1', 'checklist_group_id' => $checklist_group->id]);
+
+        $task1 = Task::factory()->create(['checklist_id' => $checklist->id, 'position' => 1]);
+        $task2 = Task::factory()->create(['checklist_id' => $checklist->id, 'position' => 2]);
+
+        Livewire::test(TasksTable::class, ['checklist' => $checklist])
+            ->call('task_up', $task2->id);
+
+        $task = Task::find($task2->id);
+        $this->assertEquals(1, $task->position);
+
+        Livewire::test(TasksTable::class, ['checklist' => $checklist])
+            ->call('task_down', $task2->id);
+
+        $task = Task::find($task2->id);
+        $this->assertEquals(2, $task->position);
     }
 }
